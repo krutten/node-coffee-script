356d1bab03b95afb7061ab952c2e30eb3baac909
Kevin Rutten <Kevin@KevinRutten.com>
2014-08-08 12:48:48 -0700

    don't emerge every deploy

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,5 +1,5 @@
 # Set the system Node.js to 0.10.28
-sudo "emerge =net-libs/nodejs-0.10.28"
+sudo "emerge -n =net-libs/nodejs-0.10.28"
 sudo "eselect nodejs set 0.10.28"
 sudo "ln -nfs /opt/nodejs/0.10.28 /opt/nodejs/current"
 
